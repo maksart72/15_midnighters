20f0ec8988166ae445df0be42adaae97cd9fc3b0
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-08-09 13:48:14 +0500

    Remove magic nums, use tuple, new load_attempts funct.

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -4,32 +4,28 @@ import pytz
 import requests
 
 
-def load_attempts():
-    pages = 10
-    all_records = []
+def load_attempts(pages):
+    all_records = ()
     for page in range(pages):
         url = 'https://devman.org/api/challenges/solution_attempts/'
         details = {'page': page + 1}
         records_request = requests.get(url, params=details)
         records_json = json.loads(records_request.content)
         for records in records_json['records']:
-            records_info = [records['username'],
-                            records['timestamp'], records['timezone']]
-            all_records.append(records_info)
-    return all_records
+            if records['timestamp'] is not None:
+                timezone = pytz.timezone(records['timezone'])
+                datetime_timezone = datetime.datetime.fromtimestamp(
+                    records['timestamp'], timezone)
+                if datetime_timezone.hour < 6:
+                    all_records = all_records + (records['username'],)
+    return set(all_records)
 
 
-def get_midnighters(attempts_information):
-    midnighters = []
-    for attempt in attempts_information:
-        if bool(attempt[1]):
-            timezone = pytz.timezone(attempt[2])
-            datetime_timezone = datetime.datetime.fromtimestamp(attempt[1], timezone)
-            if datetime_timezone.hour < 6:
-                midnighters.append(attempt[0])
-    return set(midnighters)
+def print_midnighters(attempts_information):
+    for developer in attempts_information:
+        print(developer)
+
 
 if __name__ == '__main__':
-    midnighters_set = get_midnighters(load_attempts())
-    for midnight_user in midnighters_set:
-        print(midnight_user)
+    pages_num = 10
+    print_midnighters(load_attempts(pages_num))
